d78a21e05d68015ff93cd2f726c51d48bcbe3aee
Johnny Mayer <johnnymayer@gmail.com>
2018-05-04 09:07:33 -0700

    Minor HTML edits, create ViewModels, controllers.

---
+++ PersonalPortfolio/Controllers/BlogController.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using PersonalPortfolio.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace PersonalPortfolio.Controllers
+{
+    [Authorize]
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public BlogController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Blogs.ToList();
+            return View(model);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Blog blog)
+        {
+            _db.Blogs.Add(blog);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisBlog = _db.Blogs.FirstOrDefault(Blogs => Blogs.BlogId == id);
+            return View(thisBlog);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisBlog = _db.Blogs.FirstOrDefault(Blogs => Blogs.BlogId == id);
+            _db.Blogs.Remove(thisBlog);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PersonalPortfolio/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalPortfolio.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "User Role")]
+        public string UserRoleId { get; set; }
+
+    }
+}
